65ff84c9c4137160a7d59931cef2f56f4a4d5f7d
hc8sea <hc8sea@gmail.com>
2022-03-10 15:44:04 -0300

    aef

--- application.py
+++ application.py
@@ -1,23 +1,23 @@
 import streamlit as st
 import pandas as pd
 import pymysql
-from sqlalchemy import *
-import sqlalchemy
-from sqlalchemy import create_engine, MetaData, Table, Column, Numeric, Integer, VARCHAR
-from sqlalchemy.engine import result
-from sqlalchemy import text
+#from sqlalchemy import *
+#import sqlalchemy
+#from sqlalchemy import create_engine, MetaData, Table, Column, Numeric, Integer, VARCHAR
+#from sqlalchemy.engine import result
+#from sqlalchemy import text
 import numpy as np
 import requests
 
 def main():
 
-        st.header()
+        st.text('projeto em andamento. estou atualizando direto no main. não repare a bagunça')
         credentials = 'mysql+pymysql://hc8sea:portfoliopassword@volleyball.cf69hpkhvjyq.us-east-1.rds.amazonaws.com/volleyball'
 
         df = pd.read_sql("""
                     SELECT * from volleyball.out
                     """, con = credentials)
-
+        df
 
         a = 'This will be a volleyboard dashball. Or something like that ok'
 
